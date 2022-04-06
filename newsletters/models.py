47f60d0db9604aac26bbab991c27d4dcd0e23199
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2022-04-06 01:23:33 -0400

    agregando modelos

--- newsletters/models.py
+++ newsletters/models.py
@@ -13,5 +13,13 @@ class NewsLetterUser(models.Model): #este es el modelo para el usuario
     def __str__(self):
         return self.email 
 
-class NewsLetter(models.Model): #esto es el modelo es decir lo que le llegara al usuario 
-    name = models.CharField 
+class NewsLetter(models.Model): #este modelo es el email que le llegara al usuario 
+    name = models.CharField(max_length=250) #esto es como decir el titulo del correo que haremos
+    subject = models.CharField(max_length=250)#esto es el tema de lo que hablamos en el correo
+    body = models.TextField(blank=True, null=True)#esto es el contenido del proyecto 
+    email = models.ManyToManyField(NewsLetterUser) #relacion entre modelos
+    created = models.DateTimeField(auto_now_add=True)#info para saber cuando fue creado el email 
+
+    def __str__(self):
+        return self.name
+
