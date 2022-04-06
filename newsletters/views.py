3f6919b9886a9e0a5048ff0b8e6d673688f49145
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2022-04-06 01:37:23 -0400

    agregando vistas para jalar datos de los modelos

--- newsletters/views.py
+++ newsletters/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .forms import NewsletterUserSignUpForm
+# Estas son las vistas para que los usuarios se suscriban y reciban nuestros emails 
 
-# Create your views here.
+def newsletter_signup(request):
+    form = NewsletterUserSignUpForm(request.POST or None) #esto es para jalar los datos
